b460aa0a5378faccdabd65cfc2a70bf6af88d0eb
AlexAbrello <92325674+AlexAbrello@users.noreply.github.com>
2023-03-17 20:40:32 +0300

    Add files via upload

---
+++ 01.js
@@ -0,0 +1,13 @@
+function sumTwoSmallestNumbers(numbers) {  
+   let newArr = []
+  while (newArr.length < 2) {
+    let min = Math.min(...numbers)
+    newArr.push(min)
+    numbers = numbers.filter( elem => elem != min)
+  }
+   return newArr.reduce( (acum, item) => acum + item, 0)
+}
+
+function sumTwo(numbers) {
+   
+}
